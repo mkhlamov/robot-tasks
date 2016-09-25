c3cde2de510afc958d3f7df599e36d1c7d05556f
Maxim Khlamov <mkhlamov@gmail.com>
2016-09-25 17:02:37 +0300

    Second Task

--- task_2.py
+++ task_2.py
@@ -5,7 +5,12 @@ from pyrob.api import *
 
 @task
 def task_1_2():
-    pass
+    move_right(2)
+    move_down(2)
+    fill_cell()
+    move_down()
+    move_right(2)
+
 
 
 if __name__ == '__main__':
